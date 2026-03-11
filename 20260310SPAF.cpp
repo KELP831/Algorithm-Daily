06ec0f90328e24a8d90e63ea07f0f6e5db461ae5
KELP831 <zhengkexin0831@gmail.com>
2026-03-11 16:25:57 +0800

    20260311Prim算法（朴素版）

--- 20260310SPAF.cpp
+++ 20260310SPAF.cpp
@@ -31,28 +31,33 @@
 
 using namespace std;
 
-const int N=120;
+const int N=100010;
 int dist[N];
 int n,m,INF=0x3f3f3f3f;
+bool st[N];
+int h[N],e[N],ne[N],w[N],idx;
 
-struct Edges{
-    int a;
-    int b;
-    int w;
-}edges[N];
+void add(int a,int b,int c){
+    e[idx]=b,w[idx]=c,ne[idx]=h[a],h[a]=idx++;
+}
 
 int spfa(){
     memset(dist,0x3f,sizeof dist);
+    dist[1]=0;
     queue<int> Q;
     Q.push(1);
+    st[1]=true;
     while(!Q.empty()){
         int t=Q.front();
         Q.pop();
-        for(int i=1;i<=m;i++){
-            int a=edges[i].a,b=edges[i].b,w=edges[i].w;
-            if(a==t&&dist[b]>dist[a]+w) {
-                dist[b]=dist[a]+w;
-                Q.push(b);
+        st[t]=false;
+        for(int i=h[t];i!=-1;i=ne[i]){
+            if(dist[e[i]]>dist[t]+w[i]){
+                dist[e[i]]=dist[t]+w[i];
+                if(!st[e[i]]){
+                    Q.push(e[i]);
+                    st[e[i]]=true;
+                }
             }
         }
     }
@@ -61,12 +66,82 @@ int spfa(){
 
 int main(){
     scanf("%d%d",&n,&m);
+    memset(h,-1,sizeof h);
     for(int i=1;i<=m;i++){
         int a,b,w;
         scanf("%d%d%d",&a,&b,&w);
-        edges[i]={a,b,w};
+        add(a,b,w);
     }
     int t=spfa();
     if(t==INF) puts("impossible");
     else cout<<t;
 }
+//只能使用邻接表存储
+
+//判断有无负环
+//增添数组cnt[N]来记录路径中边的数量
+//如果某个cnt[i]>=n，则说明有负环
+//所以一开始我们要将所有的顶点都加入队列
+/*
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+const int N=100010;
+int dist[N];
+int n,m,INF=0x3f3f3f3f,cnt[N];
+bool st[N];
+int h[N],e[N],ne[N],w[N],idx;
+
+void add(int a,int b,int c){
+    e[idx]=b,w[idx]=c,ne[idx]=h[a],h[a]=idx++;
+}
+
+bool spfa(){
+    memset(dist,0x3f,sizeof dist);
+    memset(cnt,0,sizeof cnt);
+    dist[1]=0;
+    queue<int> Q;
+    for (int i = 1; i <= n; i ++ )
+    {
+        st[i] = true;
+        Q.push(i);
+    }
+    while(!Q.empty()){
+        int t=Q.front();
+        Q.pop();
+        st[t]=false;
+        for(int i=h[t];i!=-1;i=ne[i]){
+            if(dist[e[i]]>dist[t]+w[i]){
+                dist[e[i]]=dist[t]+w[i];
+                cnt[e[i]]=cnt[t]+1;
+                if (cnt[e[i]] >= n) return true;
+                if (!st[e[i]])
+                {
+                    Q.push(e[i]);
+                    st[e[i]] = true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    scanf("%d%d",&n,&m);
+    memset(h,-1,sizeof h);
+    for(int i=1;i<=m;i++){
+        int a,b,w;
+        scanf("%d%d%d",&a,&b,&w);
+        add(a,b,w);
+    }
+    
+    bool flag=spfa();
+    
+    if(!flag)cout<<"No";
+    else cout<<"Yes";
+}
+*/
